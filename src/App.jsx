8eab23a1ec153b39ce864aae095b149f175e51aa
developeryyt <yeontaey92@gmail.com>
2024-03-20 23:33:45 +0900

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import tw, { styled } from "twin.macro";
-import {useState} from "react";
-
-
-const Box = styled.div(({ action }) => [
-    tw`text-3xl`,
-    action && tw`bg-black opacity-100`,
-    'color: #fff'
-])
-
-
-
 function App() {
 
-    const [a, ] = useState(true)
 
     return (
-        <Box action={a}>
-            123
-        </Box>
+        <>123</>
     )
 }
 
