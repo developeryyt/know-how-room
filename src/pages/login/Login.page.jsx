067a1b0233744c8c12e68e82a7500ff86ab081a1
developeryyt <yeontaey92@gmail.com>
2024-03-26 16:52:40 +0900

    login

--- src/pages/login/Login.page.jsx
+++ src/pages/login/Login.page.jsx
@@ -16,38 +16,48 @@ const LoginPage = () => {
         }
     })
 
+
+
     return (
-        <div className='login_wrapper make-center'>
+        <div className='login_wrapper make-center rounded-md p-20 border-1 shadow-lg'>
             <div className='login'>
                 <form onSubmit={e => {
                     e.preventDefault()
                     formik.handleSubmit()
                 }}>
 
-                    <div>
-                        <input
-                            id='userId'
-                            name='userId'
-                            value={formik.values.userId}
-                            onChange={formik.handleChange}
-                            placeholder='아이디를 입력해주세요.'
-                            type='text'
-                        />
-                    </div>
+                    <div className='flex flex-col'>
 
-                    <div>
-                        <input
-                            id='userPw'
-                            name='userPw'
-                            type='password'
-                            value={formik.values.userPw}
-                            onChange={formik.handleChange}
-                            placeholder='비밀번호를 입력해주세요.'
-                        />
-                    </div>
+                        <div className='pb-24'>
+                            <label className='block pr-12 pb-4 text-14 font-500'>아이디</label>
+                            <input
+                                id='userId'
+                                name='userId'
+                                value={formik.values.userId}
+                                onChange={formik.handleChange}
+                                placeholder='아이디를 입력해주세요.'
+                                type='text'
+                                className='ipt'
+                            />
+                        </div>
 
-                    <div>
-                        <button type='submit'>로그인</button>
+                        <div className='pb-24'>
+                            <label className='block pr-12 pb-4 text-14 font-500'>비밀번호</label>
+                            <input
+                                id='userPw'
+                                name='userPw'
+                                type='password'
+                                value={formik.values.userPw}
+                                onChange={formik.handleChange}
+                                placeholder='비밀번호를 입력해주세요.'
+                                className='ipt'
+                            />
+                        </div>
+
+                        <div>
+                            <button type='submit'>로그인</button>
+                        </div>
+
                     </div>
 
                 </form>
