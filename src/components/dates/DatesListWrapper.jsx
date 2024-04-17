a162d55af8e6612b236a15ff7b8a62e505e96256
developeryyt <yeontaey92@gmail.com>
2024-04-17 13:42:18 +0900

    ...

--- src/components/dates/DatesListWrapper.jsx
+++ src/components/dates/DatesListWrapper.jsx
@@ -2,12 +2,16 @@ import DATES from './index.js'
 import styled from "styled-components";
 import i18n from "../../utils/i18n.js";
 import {observer} from "mobx-react";
-import {useCallback} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 
 
 const DatesListWrapper = observer(() => {
 
+    const [topH, setTopH] = useState(0)
+    const info = useRef()
+
     const DateWrapper = styled.div`
+        height: 100%;
     `
 
     const DateInfo = styled.div`
@@ -18,6 +22,34 @@ const DatesListWrapper = observer(() => {
 
     const InfoWrapper = styled.div`
         display: flex;
+        height: calc(100% - ${topH}px);
+        
+        .daypick {
+            text-align: center;
+            flex-basis: 140px;
+        }
+        
+        .reserveList {
+            flex-basis: 35%;
+            padding: 0 15px;
+            .btn_wrap {
+                display: flex;
+                justify-content: flex-end;
+                gap: 6px;
+            }
+            
+        }
+        
+        .reserveDetail {
+            flex-basis: 65%;
+            border-right: 0;
+        }
+        
+        > * {
+            overflow-y: auto;
+            border-right: 1px solid black;
+            box-sizing: border-box;
+        }
         
     `
 
@@ -32,10 +64,14 @@ const DatesListWrapper = observer(() => {
     }, [DATES.selectedDate['year'], DATES.selectedDate['month']])
 
 
+    useEffect(() => {
+        setTopH(info.current.clientHeight)
+    }, [])
+
 
     return (
         <DateWrapper>
-            <DateInfo>
+            <DateInfo ref={info}>
                 <div>
                     <button
                         type='button'
@@ -60,9 +96,6 @@ const DatesListWrapper = observer(() => {
                             DATES?.nextYear()
                         }}
                     >{i18n.t("Next Year")}</button>
-
-
-
                 </div>
 
                 <div>
@@ -96,7 +129,7 @@ const DatesListWrapper = observer(() => {
                 </div>
             </DateInfo>
             <InfoWrapper>
-                <ul className='px-20 pb-10'>
+                <ul className='daypick px-20 pb-10'>
                     {
                         (DATES?.dateArr || [])?.map(({dd, ee}, idx) => {
                             let restDay = ee === '일' ? 'text-red-500' : ee === '토' ? 'text-blue-500' : ''
@@ -105,7 +138,8 @@ const DatesListWrapper = observer(() => {
                                 <li key={`day__${idx}`} className='mb-4 last:mb-0'>
                                     <button
                                         type='button'
-                                        onClick={() => {
+                                        onClick={(e) => {
+                                            console.log(e.target.textContent, dd, ee)
                                         }}
                                         className={`${restDay} text-14`}
                                     >{dd}일 ({ee})
@@ -115,6 +149,32 @@ const DatesListWrapper = observer(() => {
                         })
                     }
                 </ul>
+                <div className='reserveList'>
+                    <div className='btn_wrap'>
+                        <div>
+                            <button
+                                type='button'
+                                onClick={() => {}}
+                            >선택 항목 삭제</button>
+                        </div>
+                        <div>
+                            <button
+                                type='button'
+                                onClick={() => {}}
+                            >전체 삭제</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <ul>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <div className='reserveDetail'>
+
+                </div>
             </InfoWrapper>
         </DateWrapper>
     )
